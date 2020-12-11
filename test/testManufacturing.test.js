6cb5d9742563115bba021e4cd023c28031fe3fbe
AnthonyPageau <70658822+AnthonyPageau@users.noreply.github.com>
2020-12-11 18:31:04 -0500

    Update testManufacturing.test.js

--- test/testManufacturing.test.js
+++ test/testManufacturing.test.js
@@ -36,41 +36,5 @@ contract("Manufacturing", function() {
             })
         })
     })
-
-    describe('getting the number of buttons using getButtons', () =>{
-        it("Should return number of buttons", () => {
-            Manufacturing.deployed()
-            .then(instance =>  {
-                meta = instance;        
-                meta.getButtons.call().then(numButtons => {
-                    assert.equal(numButtons, 10);
-                });
-            })
-        })
-    })
-
-    describe('getting the number of display using getDisplay', () =>{
-        it("Should return number of display", () => {
-            Manufacturing.deployed()
-            .then(instance =>  {
-                meta = instance;        
-                meta.getDisplay.call().then(numDisplay => {
-                    assert.equal(numDisplay, 1);
-                });
-            })
-        })
-    })
-
-    describe('getting the number of batteries using getBatteries', () =>{
-        it("Should return number of batteries", () => {
-            Manufacturing.deployed()
-            .then(instance =>  {
-                meta = instance;        
-                meta.getBatteries.call().then(numBatteries => {
-                    assert.equal(numBatteries, 1);
-                });
-            })
-        })
-    })
 })
 
